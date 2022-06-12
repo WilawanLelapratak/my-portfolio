985f4319b330101d31a2e4b8df0db490bb6b9dd4
WilawanLelapratak <wilawan.wove@gmail.com>
2022-06-12 21:01:07 +0700

    Updated social

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,8 +4,8 @@ class Social extends Component {
     render() {
         return (
             <div className="social">
-                <a href="/#" target="_blank" rel="noopender noreferrer"><i class="fab fa-github"></i></a>
-                <a href="/#" target="_blank" rel="noopender noreferrer"><i class="fab fa-facebook"></i></a>
+                <a href="https://github.com/WilawanLelapratak" target="_blank" rel="noopender noreferrer"><i class="fab fa-github"></i></a>
+                <a href="https://www.facebook.com/wilawan.wove/" target="_blank" rel="noopender noreferrer"><i class="fab fa-facebook"></i></a>
             </div>
         );
     }
