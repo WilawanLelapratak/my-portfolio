4d1b7d5e8e1a348af13fa4856789872d80bc46da
WilawanLelapratak <wilawan.wove@gmail.com>
2022-06-12 20:39:14 +0700

    debuging picture doesn't show

--- src/contents/About.js
+++ src/contents/About.js
@@ -5,7 +5,7 @@ class About extends Component {
         return (
             <div className="condiv about">
                 <h1 className="subtopic">About Me</h1>
-                <img src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.18169-9/26055863_10209915097305588_8829068986009356735_n.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=DIHLqfxIPKwAX8R7ZWc&_nc_ht=scontent.fbkk22-2.fna&oh=00_AT-2jgC7IDC7_vkjPjBexH6kbcWJ-3KKiCLFVW1m4KO00A&oe=62A40C9E" alt="profile" className="profilepic" />
+                <img src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.18169-9/26055863_10209915097305588_8829068986009356735_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=f9PgE7l0J1UAX_A92T7&_nc_ht=scontent.fbkk22-2.fna&oh=00_AT8l8ZfcwUqa0zslrI1nBv5toF8a7ylmZaaarvRYaqIW9w&oe=62CB999E" alt="profile" className="profilepic" />
                 <h3>Hi, I'm Wilawan</h3>
                 <p>My career objective is to become a software tester where I have to use my skills and knowledge for the development of the project, where strong project management and analytical skill will be useful in the delivery of the services to the customer.</p>
             </div>
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-                <img src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.18169-9/26055863_10209915097305588_8829068986009356735_n.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=DIHLqfxIPKwAX8R7ZWc&_nc_ht=scontent.fbkk22-2.fna&oh=00_AT-2jgC7IDC7_vkjPjBexH6kbcWJ-3KKiCLFVW1m4KO00A&oe=62A40C9E" alt="profile" className="profilepic" />
+                <img src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.18169-9/26055863_10209915097305588_8829068986009356735_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=f9PgE7l0J1UAX_A92T7&_nc_ht=scontent.fbkk22-2.fna&oh=00_AT8l8ZfcwUqa0zslrI1nBv5toF8a7ylmZaaarvRYaqIW9w&oe=62CB999E" alt="profile" className="profilepic" />
                 <ReactTypingEffect text={['I am Wilawan', 'I am a Software Test Engineer']} speed={100} eraseDelay={300} className="typingeffect" />
                 <Social />
             </div>
